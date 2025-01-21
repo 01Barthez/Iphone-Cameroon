5cb29db77968bb6d2fd2260ba3ad67b601967b58
01Barthez <kenwoubarthez@gmail.com>
2025-01-21 16:59:35 +0100

    feat: Add testimony part

--- src/components/custom/Testimonies.tsx
+++ src/components/custom/Testimonies.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Testimonies: React.FC = () => {
     return (
-        <div>
-            Testimonies part
-        </div>
+        <section className='container py-6 md:py-10 lg:py-12 flex flex-col gap-6 items-center'>
+            <h3 className="text-center text-2xl md:text-3xl lg:text-4xl font-semibold">
+                Hear from our customers
+            </h3>
+
+            <p className="max-w-full md:max-w-[85%] text-center text-sm md:text-base text-foreground/90 ">
+                At IPhone Cameroun, quality isn't just a word - it's our promise.
+                All our iPhones, new and used, undergo strict quality control to ensure they meet the highest standards.
+                We understand the importance of having a reliable device, which is why we are committed
+                to supplying only products in perfect working order.
+            </p>
+
+            {/* Testimony part */}
+        </section>
     )
 }
 
---
+++ src/core/mocks/mock-testimony.tsx
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const mockTestimony = [
+    {
+        id: uuidv4(),
+        profile: "public/images/ipad1.svg",
+        name: "Martin escobar",
+        testimony: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et est hendrerit, porta nunc vitae, gravida justo. Nunc fermentum magna lorem, euismod volutpat arcu volutpat"
+    },
+    {
+        id: uuidv4(),
+        profile: "public/images/ipad1.svg",
+        name: "Martin escobar",
+        testimony: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et est hendrerit, porta nunc vitae, gravida justo. Nunc fermentum magna lorem, euismod volutpat arcu volutpat"
+    },
+]
